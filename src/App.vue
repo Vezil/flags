216c0d46a2aeb2b2cfbce1fbf35770c4131976a8
Vezil <SzymonWojaczek@wp.pl>
2019-08-19 14:08:03 +0200

    Part 1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,118 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h2>Vue Autocomplete</h2>
+    <switch-theme />
+    <p>
+      Selected Timezone: <b>{{ selectedTimezone || 'Not chosen'}}</b>
+    </p>
+    <cool-select v-model="selectedTimezone" :items="items"  item-value="name" item-text="name">
+        <template slot="item" slot-scope="{ item : timezone }">
+            <div style="display:flex;align-items:center;">
+              <img :src="getFlag(timezone.countryCode)" class="country-flag">
+                <div>{{ timezone.offset }}<b> {{ timezone.name }}</b></div>
+              </div>
+        </template>
+
+        <template slot="selection" slot-scope="{ item :timezone }">
+              <img :src="getFlag(timezone.countryCode)" class="country-flag">
+                <div>{{ timezone.offset }}<b> {{ timezone.name }}</b></div>
+              
+        </template>
+    
+    </cool-select>
+    <button style="margin-top:15px;" @click="setRandom">
+      Set random value
+      </button>
+
+
+   <!-- <span slot="error" slot-scope="{ errorMessage }">
+      Error: {{errorMessage}}
+    </span>-->
   </div>
 </template>
+<script>
+  import { CoolSelect } from 'vue-cool-select'
+  import SwitchTheme from './components/SwitchTheme.vue'
+  import countries from "./assets/countries"
+  export default {
+    name:'App',
+    components: {
+        CoolSelect,
+        SwitchTheme,
+      
+    },
+    data() {
+      return {
+            selected: null,
+            items: countries.data,
+            selectedTimezone:'Asia/Jakarta'
+            //selectedCountry: 'India'
+           // errorMessage: null
+      }
+    },
+    // watch: {
+      // selected() {
+        // this.validate();
+      // }
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    // },
+    methods: { 
+      // validate() {
+        // this.errorMessage = !this.selected ? "this is required field!" : null;
+      // },
+      getFlag (flagName) {
+        try{
+          return require(`./assets/flags/${flagName.toLowerCase()}.svg`)
+        } catch (e) {
+          return require('./assets/flags/undefined.svg')
+        }
+      },
+      setRandom() {
+        const randomIndex = Math.floor(Math.random()*this.items.length)
+        this.selectedTimezone = this.items[randomIndex].name
+      }
     }
   }
+</script>
+
+<style lang="scss" scoped>
+.country-flag {
+  max-width:30px;
+  margin-right: 10px;
+  border: 1px solid #eaecf0;
 }
+
+#app {
+  height: 100vh;
+}
+
+#app > div {
+  width: 450px;
+}
+
+
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
+import VueSelect from 'vue-cool-select'
 import App from './App.vue'
 import router from './router'
+
+const theme = window.location.hash.slice(1) || 'bootstrap';
+
+Vue.use(VueSelect, {
+  theme: theme
+})
 
 Vue.config.productionTip = false
 
@@ -8,3 +15,6 @@ new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+
+
